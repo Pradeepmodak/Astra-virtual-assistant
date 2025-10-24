72e0f05e811870e127b49599452a7b73f7999c85
pradeepmodakofficial@gmail.com <pradeepmodakofficial>
2025-10-24 10:30:47 +0530

    Delete all files

--- 10todocontextLocal/src/components/index.js
+++
@@ -1,4 +0,0 @@
-import TodoForm from "./TodoForm";
-import TodoItem from "./TodoList";
-
-export { TodoForm, TodoItem };
--- Leetcode/backend/src/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-
-async function main(){
-    // console.log("Connecting to DB...");
-    await mongoose.connect(process.env.DB_CONNECT_STRING);
-}
-
-module.exports=main;
